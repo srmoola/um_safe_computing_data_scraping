08453df724df3a133c17cda6c12b919d40b758a2
Satyadev Moolagani <smoolagani@gmail.com>
2024-06-26 17:21:03 -0400

    added bash script to run all python files in order

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DATA_FOLDER_PATH="site_data"
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+echo "Getting Site Links"
+python scripts/get_links.py
+
+echo "Starting Scraping Process"
+python scrapers/async_scrape.py
+
+echo "Uploading to Google Drive"
+python scripts/upload_to_google_drive.py $DATA_FOLDER_PATH
+
