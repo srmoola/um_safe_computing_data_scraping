47a65c2410c5facc2d4bf2f148f40e268c8f528e
Satyadev Moolagani <smoolagani@gmail.com>
2024-07-31 14:24:19 -0400

    added gh pages

--- embed-prototype/vite.config.ts
+++ embed-prototype/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/um_safe_computing_data_scraping",
   plugins: [react()],
-})
+});
